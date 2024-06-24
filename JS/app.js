5ac55691e773bf10d65df84cdd18f3e8af1fb3da
YGainey <yousef.gainey@gmail.com>
2024-06-24 16:20:07 +0100

    completed - mineCounter(), moverCounter(), buildBoard(), placeMines() ->error here, buildBoardArray(), and visually representing the array in the browser - no styling currently

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,84 @@
-//FIB(I) = FIB(I-1) + FIB(I-2)
+/*-------------------------------- Constants --------------------------------*/
+const board = []
 
-const fib = [0 , 1]
+//Building the array that based on difficulty
+// const easyWidthHeight = 10
+const mediumWidthHeight = 16
+// const hardWidth = 24
+// const hardHeight = 16
+// const hard = hardWidth * hardHeight 
 
-for (let i = 2; i < 100; i++){
-    fib[i] = fib[i-1] + fib[i-2]
-    console.log(fib[i])
+// Number of mines per difficulty
+// const numOfMinesEasy = 10
+const numOfMinesMedium = 40
+// const numOfMinesHard = 99
+
+/*---------------------------- Variables (state) ----------------------------*/
+let moves = 0
+let deathCondition = false
+
+/*------------------------ Cached Element References ------------------------*/
+const grid = document.querySelector('.grid')
+
+/*-------------------------------- Functions --------------------------------*/
+// function init(){
+
+// }
+// function render(){
+
+// }
+
+
+
+function buildBoardArray(){
+    for(let i = 0; i < mediumWidthHeight**2; i++)
+        board[i] = '0'
 }
+buildBoardArray()
+
+function placeMines(){
+    let i
+    for (i = 0; i <= numOfMinesMedium; i++){
+        let j = Math.floor(Math.random() * 16)
+        let k = Math.floor(Math.random() * 16)
+        while(board[j][k] !== ''){
+            j = Math.floor(Math.random() * 16)
+            k = Math.floor(Math.random() * 16)
+        }
+        // console.log(`i: ${i} j: ${j}`)
+        // console.log(`i: ${i} j: ${k}`)
+        board[j][k] = 'X'
+    }
+    // console.log(board)
+}
+placeMines()
+
+function buildBoard(){
+    board.forEach((sqr, i) =>{
+        const square = document.createElement('div')
+        square.innerText = board[i]
+        square.classList.add('sqr')
+        square.id = i
+        grid.appendChild(square)
+    })
+}
+buildBoard()
+
+
+
+// function moverCounter(){
+//     moves++
+//     console.log(moves)
+// }
+// moveCounter()
+
+
+// function mineCounter(){
+//     numOfMines--
+//     console.log(numOfMines)
+// }
+//mineCounter()
+
+// init()
+/*----------------------------- Event Listeners -----------------------------*/
 

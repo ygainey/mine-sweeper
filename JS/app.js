103789cac6b24518cfd9a022e170b112c6b876b1
YGainey <yousef.gainey@gmail.com>
2024-06-25 19:40:04 +0100

    left+right click added, additional styling

--- JS/app.js
+++ JS/app.js
@@ -16,9 +16,19 @@ const numOfMinesMedium = 40
 /*---------------------------- Variables (state) ----------------------------*/
 let moves = 0
 let deathCondition = false
+let flagCounter = numOfMinesMedium
 
 /*------------------------ Cached Element References ------------------------*/
 const grid = document.querySelector('.grid')
+console.dir(grid)
+// const cell = document.querySelectorAll('.sqr')
+// console.dir(cell)
+const reset = document.querySelector('.reset')
+console.dir(reset)
+const flagCounterEl = document.querySelector('.flagCounter')
+console.dir(flagCounter)
+const moveCounterEl = document.querySelector('.moveCounter')
+console.dir(moveCounter)
 
 /*-------------------------------- Functions --------------------------------*/
 // function init(){
@@ -29,18 +39,20 @@ const grid = document.querySelector('.grid')
 // }
 
 
-
+//build the array representing the board
 function buildBoardArray(){
     for(let i = 0; i < mediumWidthHeight**2; i++)
-        board[i] = '.'
+        board[i] = ' '
 }
 buildBoardArray()
+// console.log(board)
 
+//randomly place mines in the board array
 function placeMines(){
     for (let i = 0; i < numOfMinesMedium; i++){
-        let j = Math.floor(Math.random() * board.length-1)
-        while(board[j] !== '.'){
-            j = Math.floor(Math.random() * board.length-1)
+        let j = Math.floor(Math.random() * board.length)
+        while(board[j] !== ' '){
+            j = Math.floor(Math.random() * board.length)
         }
         // console.log(`i: ${i} j: ${j}`)
         // console.log(`i: ${i} j: ${k}`)
@@ -50,6 +62,7 @@ function placeMines(){
 }
 placeMines()
 
+//calculate the number of adjacent mines to a square
 function adjacentMines(){
     board.forEach((sqr, i) => {
         let mineCount = 0
@@ -78,40 +91,81 @@ function adjacentMines(){
             if(i % 16 !== 15 && board[i+17] === 'X'){
                 mineCount++
             }
-            board[i] = mineCount
+            if(mineCount > 0){
+                board[i] = mineCount
+            }
         }
     })
 }
-adjacentMines()
+//adjacentMines()
 
+//update the move counter on the UI
+function moveCounter(){
+    moveCounterEl.innerText = moves
+}
+// moveCounter()
+
+//update the mine/flag counter on the UI
+function mineCounter(){
+    flagCounterEl.innerText = flagCounter 
+}
+// mineCounter()
+
+//floodfill for clicked sqaure
+function flood(){
+    console.log('floodfill sim')
+}
+
+//dynamically build the board & event listener for left and right clicks
 function buildBoard(){
     board.forEach((sqr, i) =>{
         const square = document.createElement('div')
         square.innerText = board[i]
+        // console.log(board)
         square.classList.add('sqr')
         square.id = i
         grid.appendChild(square)
+        adjacentMines()
+        mineCounter()
+        moveCounter()
+        square.addEventListener('click', leftClick)
+        square.addEventListener('contextmenu', rightClick)
+        function rightClick(event){
+            event.preventDefault()
+            console.log('right')
+            if(square.classList.contains('flag')){
+                square.classList.remove('flag')
+                flagCounter++
+                mineCounter()
+            }else{
+                square.classList.add('flag')
+                flagCounter--
+                mineCounter()
+            }
+        }
+        function leftClick(){
+            let idx = square.getAttribute('id')
+            parseInt(idx)
+            if(board[idx] === 'X'){
+                console.log('you are dead')
+                moves++
+                moveCounter() // probably not needed later
+            }else if(square.classList.contains('flag')){
+                //do nothing
+            }else{
+                flood()
+                moves++
+                moveCounter() //probably not needed later
+            }
+        }     
     })
 }
 buildBoard()
 
-// function floodFill(){
-//     if
-// }
-
-// function moverCounter(){
-//     moves++
-//     console.log(moves)
-// }
-// moveCounter()
-
-
-// function mineCounter(){
-//     numOfMines--
-//     console.log(numOfMines)
-// }
-//mineCounter()
-
 // init()
 /*----------------------------- Event Listeners -----------------------------*/
 
+// cell.forEach((sqr) =>{
+//     sqr.addEventListener('click', leftClick)
+//     sqr.addEventListener('oncontextmenu', rightClick)
+// })
